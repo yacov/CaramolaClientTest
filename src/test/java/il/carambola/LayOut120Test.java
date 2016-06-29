27639f3fd738c2caee67de99607231f2a9a3a4e5
Iakov Volf <jakoff@gmail.com>
2016-06-29 12:19:30 +0300

    Added init browser method to test class

--- src/test/java/il/carambola/LayOut120Test.java
+++ src/test/java/il/carambola/LayOut120Test.java
@@ -4,33 +4,72 @@ import il.carambola.pages.Layout_120_Page;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import org.testng.asserts.SoftAssert;
 
 import static org.testng.AssertJUnit.assertTrue;
 
-public class LayOut120Test extends TestNgTestBase {
+public class LayOut120Test {
   private static Logger Log = Logger.getLogger(LogLog4j.class.getName());
     public WebDriver driver;
-
+    public Layout_120_Page layout_120_page;
   private SoftAssert softAssert = new SoftAssert();
 
-
+    @Parameters("browser")
     @BeforeClass(alwaysRun = true)
-  public void initbrowser() {
+    public void initbrowser(String browser) throws Exception {
+        if (browser.equalsIgnoreCase("firefox")) {
+
+            //create firefox instance
+
+            driver = new FirefoxDriver();
+
+        }
+
+        //Check if parameter passed as 'chrome'
+
+        else if (browser.equalsIgnoreCase("chrome")) {
+
+            //set path to chromedriver.exe You may need to download it from http://code.google.com/p/selenium/wiki/ChromeDriver
+
+
+            //create chrome instance
+
         driver = new ChromeDriver();
-        layout_120_page = PageFactory.initElements(driver, Layout_120_Page.class);
+
+        } else if (browser.equalsIgnoreCase("ie")) {
+
+            //set path to IEdriver.exe You may need to download it from
+
+            // 32 bits http://selenium-release.storage.googleapis.com/2.42/IEDriverServer_Win32_2.42.0.zip
+
+            // 64 bits http://selenium-release.storage.googleapis.com/2.42/IEDriverServer_x64_2.42.0.zip
+
+            //  System.setProperty("webdriver.ie.driver","C:\\IEdriver.exe");
+
+            //create chrome instance
+
+            driver = new InternetExplorerDriver();
+
+        } else {
+
+            //If no browser passed throw exception
+
+            throw new Exception("Browser is not correct");
+
+        }
+        // driver = new ChromeDriver();
+
 
 
   }
 
     @BeforeMethod(alwaysRun = true)
   public void initPageObjects() {
-
+        layout_120_page = PageFactory.initElements(driver, Layout_120_Page.class);
 
   }
 
@@ -38,7 +77,7 @@ public class LayOut120Test extends TestNgTestBase {
 
   //This test runs several times, every iteration with new url. Urls are stored in resources/urlList.data file
 // Test Case 1
-  @Parameters("browser")
+  //@Parameters("browser")
   @Test(dataProviderClass = DataProviders.class, dataProvider = "Urls")
   public void BasicFullLoad(String url) {
 
@@ -94,11 +133,11 @@ public class LayOut120Test extends TestNgTestBase {
 
     //assertTrue("Yes button do not exist", layout_120_page.CheckThatYesButtonExists());
   }
-}
 
-  /*@AfterClass(alwaysRun = true)
+
+    @AfterClass(alwaysRun = true)
   public void tearDown() {
     this.driver.quit();
   }
-}*/
+}
 
